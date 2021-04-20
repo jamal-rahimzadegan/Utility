74fb0399483b10b9ab85b344a6170d4ecfa8930e
Jamal Rahimzadegan <52101670+jamal-rahimzadegan@users.noreply.github.com>
2021-04-20 13:57:10 +0430

    improve> performance

--- src/convert-to-eng.ts
+++ src/convert-to-eng.ts
@@ -1,17 +1,18 @@
+type RegionType = 'ar' | 'per' | 'both';
 
-function convertToEng(txt:string, type:string = "per"):string {
- const AR_MATCH = /[٠١٢٣٤٥٦٧٨٩]/g;
- const PER_MATCH = /[۰۱۲۳۴۵۶۷۸۹]/g;
- const replacePer = (char) => char.charCodeAt(0) - 1776;
- const replaceArabic = (char) => char.charCodeAt(0) - 1632;
+const AR_MATCH = /[٠١٢٣٤٥٦٧٨٩]/g;
+const PER_MATCH = /[۰۱۲۳۴۵۶۷۸۹]/g;
+const replacePer = (char) => char.charCodeAt(0) - 1776;
+const replaceArabic = (char) => char.charCodeAt(0) - 1632;
 
- if (!txt) return "";
+function convertToEng(txt: string, type: RegionType = 'per'): string {
+  if (!txt) return '';
 
- if (type === "per") return txt.replace(PER_MATCH, replacePer);
+  if (type === 'per') return txt.replace(PER_MATCH, replacePer);
 
- if (type === "ar") return txt.replace(AR_MATCH, replaceArabic);
+  if (type === 'ar') return txt.replace(AR_MATCH, replaceArabic);
 
- if (type === "both") {
-   return txt.replace(AR_MATCH, replaceArabic).replace(PER_MATCH, replacePer);
- }
+  if (type === 'both') {
+    return txt.replace(AR_MATCH, replaceArabic).replace(PER_MATCH, replacePer);
+  }
 }
