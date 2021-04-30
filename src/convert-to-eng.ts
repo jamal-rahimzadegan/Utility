c0fa64ddeeb1649e714d23c4991466803964a138
Jamal Rahimzadegan <52101670+jamal-rahimzadegan@users.noreply.github.com>
2021-04-30 22:48:40 +0430

    Improve: types

--- src/convert-to-eng.ts
+++ src/convert-to-eng.ts
@@ -1,18 +1,19 @@
 type RegionType = 'ar' | 'per' | 'both';
 
-const AR_MATCH = /[٠١٢٣٤٥٦٧٨٩]/g;
-const PER_MATCH = /[۰۱۲۳۴۵۶۷۸۹]/g;
-const replacePer = (char) => char.charCodeAt(0) - 1776;
-const replaceArabic = (char) => char.charCodeAt(0) - 1632;
+const AR_MATCH: RegExp = /[٠١٢٣٤٥٦٧٨٩]/g;
+const PER_MATCH: RegExp = /[۰۱۲۳۴۵۶۷۸۹]/g;
 
-function convertToEng(txt: string, type: RegionType = 'per'): string {
+const replacePer = (char) => char.charCodeAt(0) - 1776 + '';
+const replaceAr = (char) => char.charCodeAt(0) - 1632 + '';
+
+export default function convertToEng(txt: string, type: RegionType = 'per'): string {
   if (!txt) return '';
 
   if (type === 'per') return txt.replace(PER_MATCH, replacePer);
 
-  if (type === 'ar') return txt.replace(AR_MATCH, replaceArabic);
+  if (type === 'ar') return txt.replace(AR_MATCH, replaceAr);
 
   if (type === 'both') {
-    return txt.replace(AR_MATCH, replaceArabic).replace(PER_MATCH, replacePer);
+    return txt.replace(AR_MATCH, replaceAr).replace(PER_MATCH, replacePer);
   }
 }
