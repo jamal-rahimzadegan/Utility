dc771cbb6b22b5ed6b20867d4bf858d2cfee0cc6
Jamal Rahimzadegan <52101670+jamal-rahimzadegan@users.noreply.github.com>
2021-08-07 16:35:52 +0430

    Update simple-axios-module.js

--- src/simple-axios-module.js
+++ src/simple-axios-module.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-function httpRequest(method, api, data = {}, headers) {
+function createAxiosRequest(method, api, data = {}, headers) {
   return axios({
     method,
-    baseURL: "https://jsonplaceholder.typicode.com/",
-    url: api, // endpoint of the data like: comments (without leading /)
+    baseURL: "baseUrl", 
+    url: api, 
     data,
     headers: {
       ...headers,
@@ -12,28 +12,31 @@ function httpRequest(method, api, data = {}, headers) {
       "Content-Type": "application/json",
     },
   })
-    .then((res) => res.data)
+    .then(({ data, status }) => {
+      if (status >= 200 && status <= 300) return data;
+      else throw api;
+    })
     .catch((e) => e);
 }
 
 export default {
   get(api, data, headers) {
-    return httpRequest("get", api, data, headers);
+    return createAxiosRequest("get", api, data, headers);
   },
 
   post(api, data, headers) {
-    return httpRequest("post", api, data, headers);
+    return createAxiosRequest("post", api, data, headers);
   },
 
   delete(api, data, headers) {
-    return httpRequest("delete", api, data, headers);
+    return createAxiosRequest("delete", api, data, headers);
   },
 
   put(api, data, headers) {
-    return httpRequest("put", api, data, headers);
+    return createAxiosRequest("put", api, data, headers);
   },
 
   patch(api, data, headers) {
-    return httpRequest("patch", api, data, headers);
+    return createAxiosRequest("patch", api, data, headers);
   },
 };
