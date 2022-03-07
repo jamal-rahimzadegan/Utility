8cd00a44827a12567676c59502637f38b3a538f4
Jamal <jamalrahimzadegan@gmail.com>
2022-03-08 00:51:20 +0330

    Add: getInternetStatus

---
+++ src/check-netowrk-status.ts
@@ -0,0 +1,12 @@
+type StatusType = 'online' | 'offline' | 'unknown';
+
+export default async function getInternetStatus(): Promise<StatusType> {
+  if (typeof document === 'undefined') return 'unknown';
+
+  try {
+    const { status } = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    return status >= 200 && status < 300 ? 'online' : 'offline'; //  true or false
+  } catch (err) {
+    return 'offline';
+  }
+}
