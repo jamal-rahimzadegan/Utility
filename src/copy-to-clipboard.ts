4843c8d5bd90c40ff4fd1fea4f87c5f471fd8a24
Jamal <jamalrahimzadegan@gmail.com>
2022-07-23 16:34:26 +0300

    Create copy-to-clipboard.ts

---
+++ src/copy-to-clipboard.ts
@@ -0,0 +1,27 @@
+
+function handleCopy() {
+	// This will be executed if the default method is not supported by the browser
+	const retryCopy = (text: string) => {
+		let input = document.createElement("input");
+		input.setAttribute("value", text);
+		document.body.appendChild(input);
+		input.select();
+		document.execCommand("copy"); // Todo: update it :)
+		document.body.removeChild(input);
+	};
+
+	return (text: string) => {
+		if (!text) return console.error(`---Pass correct text to copy --->`);
+
+		try {
+			navigator?.clipboard?.writeText(text);
+		} catch (err) {
+			console.error("err in copyToClipboard ", err);
+			retryCopy(text);
+		}
+	};
+}
+
+const copyToClipboard = handleCopy();
+export default copyToClipboard;
+
