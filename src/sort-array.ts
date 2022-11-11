44ecf648a5fb0729a6ea2c8e1d4c9e97cd76290d
Jamal <jamalrahimzadegan@gmail.com>
2022-11-11 16:54:48 +0300

    Update sort-array.ts

--- src/sort-array.ts
+++ src/sort-array.ts
@@ -1,12 +1,38 @@
-export default function sortArray(arr: any[], order: 'des' | 'asc', property?: string) {
-  // sort by property
-  let newArr = [];
-  if (property) {
-    if (order === 'asc') newArr = arr.sort((a, b) => a[property] - b[property]);
-    if (order === 'des') newArr = arr.sort((a, b) => b[property] - a[property]);
-  } else {
-    if (order === 'asc') newArr = arr.sort((a, b) => a - b);
-    if (order === 'des') newArr = arr.sort((a, b) => b - a);
+
+type Data = any[]
+type Order = 'asc' | 'desc' | 'reverse'
+type ReturnedFn = (data: Data, order?: Order, key?: string) => Data
+
+// TODO: check for arr of strings
+
+function sortBy(): ReturnedFn {
+  let sortedArr = []
+  const options = { numeric: true }
+  const locales = 'en'
+
+  const compare = (x: any, y: any, key: string) => {
+    if (!key) return
+    // if (!key) return x - y // TODO: improve types
+    return x[key].toString().localeCompare(y[key], locales, options)
   }
-  return newArr;
+
+  return (data = [], order = 'asc', key = '') => {
+    sortedArr = [...data]
+
+    switch (order) {
+      case 'asc':
+        return sortedArr.sort((a, b) => compare(a, b, key))
+      case 'desc':
+        return sortedArr.sort((a, b) => compare(b, a, key))
+      case 'reverse':
+        alert('revese')
+        return sortedArr.reverse()
+      default:
+        return sortedArr
+    }
+  }
 }
+
+const sortCustom = sortBy()
+
+export default sortCustom
