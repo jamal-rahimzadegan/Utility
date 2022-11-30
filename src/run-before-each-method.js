674cf4552674a0dac08db91d22dceb7a90a445fe
Jamal <jamalrahimzadegan@gmail.com>
2022-11-30 12:35:07 +0300

    Update run-before-each-method.js

--- src/run-before-each-method.js
+++ src/run-before-each-method.js
@@ -1,19 +1,23 @@
 /**
- Helper to call a function before each class method
- use it like below in the constructor: runBeforeEachMethod(this, this.METHOD_TO_CALL.bind(this));
- @requires class and a callback fn
- @returns a proxy
+ Utility to call a function before each class method (not getters)
+ Use it like thi in the constructor --> return runBeforeEachMethod(this, this.[METHOD_TO_CALL].bind(this));
+ @requires a Class and a Callback fn
+ @returns a Proxy
  */
 
 export default function runBeforeEachMethod(targetClass, cb) {
+  const get = (targetClass, property) => {
+    const isCallingMethod = typeof targetClass[property] === 'function'
+
+    if (!isCallingMethod) return targetClass[property]
+
+    return function (...args) {
+      cb?.(property)
+      targetClass[property].apply(targetClass, args)
+    }
+  }
+
   return new Proxy(targetClass, {
-    get(targetClass, property) {
-      if (typeof targetClass[property] === 'function') {
-        return function (...args) {
-          cb?.(property);
-          targetClass[property].apply(targetClass, args);
-        };
-      } else return targetClass[property];
-    },
-  });
+    get,
+  })
 }
