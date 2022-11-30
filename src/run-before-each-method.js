b052e06dfad3ed2e5cddf10ec6b3ae71ba3edff5
Jamal <jamalrahimzadegan@gmail.com>
2022-11-30 12:57:10 +0300

    Update: add setter

--- src/run-before-each-method.js
+++ src/run-before-each-method.js
@@ -1,5 +1,5 @@
 /**
- Utility to call a function before each class method (not getters)
+ Utility to call a function before each class method (working with getters and setters)
  Use it like thi in the constructor --> return runBeforeEachMethod(this, this.[METHOD_TO_CALL].bind(this));
  @requires a Class and a Callback fn
  @returns a Proxy
@@ -7,17 +7,22 @@
 
 export default function runBeforeEachMethod(targetClass, cb) {
   const get = (targetClass, property) => {
-    const isCallingMethod = typeof targetClass[property] === 'function'
-
-    if (!isCallingMethod) return targetClass[property]
+    const isCallingMethod = typeof targetClass[property] === "function";
+    if (!isCallingMethod) return targetClass[property];
 
     return function (...args) {
-      cb?.(property)
-      targetClass[property].apply(targetClass, args)
-    }
-  }
+      cb?.(property);
+      targetClass[property].apply(targetClass, args);
+    };
+  };
+
+  const set = (targetClass, property, value) => {
+    cb?.(property);
+    targetClass[property] = value;
+  };
 
   return new Proxy(targetClass, {
     get,
-  })
+    set
+  });
 }
