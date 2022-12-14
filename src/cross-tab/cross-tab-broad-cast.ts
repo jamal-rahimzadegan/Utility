273dc52a269a8077739a24c6634a6872eb115e71
Jamal <jamalrahimzadegan@gmail.com>
2022-12-14 16:38:24 +0300

    Create cross-tab-broad-cast.ts

---
+++ src/cross-tab/cross-tab-broad-cast.ts
@@ -0,0 +1,37 @@
+
+type BroadcastCallback = (this: BroadcastChannel, e: MessageEvent) => any
+
+class CrossTabBroadcast {
+  private readonly bc: BroadcastChannel
+  private msg: MessageEvent | null
+
+  constructor(name: string = 'cross-tab-bc') {
+    this.msg = null
+    this.bc = new BroadcastChannel(name)
+    this.bc.onmessage = (msg:MessageEvent) => (this.msg = msg)
+  }
+
+  get channel() {
+    return this.bc
+  }
+
+  sendMsg(msg: any) {
+    this.bc.postMessage(msg)
+  }
+
+  get incomingMsg() {
+    return this.msg
+  }
+
+  close() {
+    this.bc.close()
+  }
+}
+
+export default new CrossTabBroadcast()
+
+/**
+ Usage:
+ In tab-1 --> crossTab.sendMsg("Sent from class");
+ In tab-2 --> console.log(`---sent msg----> `, crossTab.incomingMsg);
+ *  */
