0ef5bff52a8c53cb9107a46b6093b32ee74456d3
Jamal <jamalrahimzadegan@gmail.com>
2022-12-26 23:36:52 +0300

    Update simple-fetch.js

--- src/simple-fetch.js
+++ src/simple-fetch.js
@@ -13,17 +13,27 @@ class HttpRequest {
       const response = await fetch(this.baseUrl + endPoint, {
         method: method.toUpperCase(),
         headers: {
-          "Content-Type": "application/json",
+          "Content-Type": "application/json"
         },
-        ...(data && { body: JSON.stringify(data) }),
+        ...(data && { body: JSON.stringify(data) })
       });
 
       return response.json();
     } catch (error) {
       return {
-        msg: `Erro in http request for ${endPoint}`,
-        error,
+        msg: `Error in http request for ${endPoint}`,
+        error
       };
     }
   }
 }
+
+const httpRequest = new HttpRequest();
+export httpRequest
+
+
+// Usage:
+//  const getData = async () => {
+//     const res = await httpRequest.get("https://jsonplaceholder.typicode.com/todos/1");
+//     console.log(res);
+//   };
