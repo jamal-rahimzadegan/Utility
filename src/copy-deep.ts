ab986a33a9bfa3503e34e86f1616142cdbc7324b
Jamal <jamalrahimzadegan@gmail.com>
2022-12-26 23:12:30 +0300

    Update copy-deep.ts

--- src/copy-deep.ts
+++ src/copy-deep.ts
@@ -1,15 +1,16 @@
 // for one level object or array use `the spread operator` and not his function. eg -> obj={ name : "" }
 // use this function for nested objects and arrays. eg -> obj1 = { obj2 : {...} }
 
-type DataType = { [key: string]: any } | any[];
+type Param = {} | any[]
 
-export default function copyDeep(value: DataType): DataType {
-	if (!value) return value;
-	if (Array.isArray(value)) return value.map((item) => item);
-	else {
-		const copiedObj = new (value as any).constructor();
-		const keys = Object.keys(value);
-		keys.forEach((key) => (copiedObj[key] = value[key]));
-		return copiedObj;
-	}
+export default function cloneDeep(param: Param): Param {
+  if (!param) return param
+
+  if (Array.isArray(param)) return param.map((item) => item)
+
+  const clonedObj = new (param as any).constructor()
+  const paramKeys = Object.keys(param)
+  paramKeys.forEach((key) => (clonedObj[key] = param[key]))
+
+  return clonedObj
 }
