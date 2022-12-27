43fd90cd3c39d1e62333a716379d979fbb9c7d10
Jamal <jamalrahimzadegan@gmail.com>
2022-12-27 12:20:24 +0300

    Update copy-to-clipboard.ts

--- src/copy-to-clipboard.ts
+++ src/copy-to-clipboard.ts
@@ -1,27 +1,20 @@
-
-function handleCopy() {
-	// This will be executed if the default method fails (is not supported by the browser).
-	const retryCopy = (text: string) => {
-		let input = document.createElement("input");
-		input.setAttribute("value", text);
-		document.body.appendChild(input);
-		input.select();
-		document.execCommand("copy"); // Todo: update it :)
-		document.body.removeChild(input);
-	};
-
-	return (text: string) => {
-		if (!text) return console.error(`---Pass correct text to copy --->`);
-
-		try {
-			navigator.clipboard.writeText(text);
-		} catch (err) {
-			console.error("err in copyToClipboard ", err);
-			retryCopy(text);
-		}
-	};
+const runFallbackCopy = (text: string) => {
+  let input = document.createElement('input')
+  input.setAttribute('value', text)
+  document.body.appendChild(input)
+  input.select()
+  document.execCommand('copy')
+  document.body.removeChild(input)
 }
 
-const copyToClipboard = handleCopy();
-export default copyToClipboard;
+export default function copyToClipboard(text: string) {
+  if (!text) return console.error(`---Pass correct text to copy --->`)
 
+  try {
+    navigator.clipboard.writeText(text)
+  } catch (err) {
+    console.error('err in copyToClipboard ', err)
+    console.info('Try to copy using fallback method')
+    runFallbackCopy(text)
+  }
+}
