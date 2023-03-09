e563ffc9d582ec365f427d4a50ce65095c5332ca
Jamal <jamalrahimzadegan@gmail.com>
2023-03-09 17:57:38 +0000

    Update simple-fetch.js

--- src/simple-fetch.js
+++ src/simple-fetch.js
@@ -1,39 +1,38 @@
-class HttpRequest {
-  constructor() {
-    this.baseUrl = "";
-    this.methods = ["post", "get"];
+class HttpClient {
+    constructor() {
+        this.baseUrl = "";
+        this.methods = ["post", "get"];
 
-    this.methods.forEach(
-      (method) => (this[method] = this.createRequest.bind(this, method))
-    );
-  }
-
-  async createRequest(method = "post", endPoint = "", data) {
-    try {
-      const response = await fetch(this.baseUrl + endPoint, {
-        method: method.toUpperCase(),
-        headers: {
-          "Content-Type": "application/json"
-        },
-        ...(data && { body: JSON.stringify(data) })
-      });
-
-      return response.json();
-    } catch (error) {
-      return {
-        msg: `Error in http request for ${endPoint}`,
-        error
-      };
+        this.methods.forEach(
+            (method) => (this[method] = this.createRequest.bind(this, method))
+        );
     }
-  }
+
+    async createRequest(method = "post", endPoint = "", data) {
+        try {
+            const response = await fetch(this.baseUrl + endPoint, {
+                method: method.toUpperCase(),
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                ...(data && { body: JSON.stringify(data) })
+            });
+
+            return response.json();
+        } catch (error) {
+            return {
+                msg: `Error in http request for ${endPoint}`,
+                error
+            };
+        }
+    }
 }
 
-const httpRequest = new HttpRequest();
-export httpRequest
-
+const httpClient = new HttpClient();
+export httpClient
 
 // Usage:
 //  const getData = async () => {
-//     const res = await httpRequest.post("SOME_API" , {userName:"test"});
+//     const res = await httpClient.post("SOME_API" , {userName:"test"});
 //     console.log(res);
 //   };
