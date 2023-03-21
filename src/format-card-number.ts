9fd643927c8ba1873d304fed8332f404a9137eb2
Jamal <jamalrahimzadegan@gmail.com>
2023-03-21 16:37:44 +0000

    Update format-card-number.ts

--- src/format-card-number.ts
+++ src/format-card-number.ts
@@ -1,9 +1,9 @@
-const MATCHER = /^(\d{0,4})(\d{0,4})(\d{0,4})(\d{0,4})$/g
+const CARD_NUMBER_REGEX = /^(\d{1,4})(\d{1,4})(\d{1,4})(\d{1,4})$/
 
 export default function formatCardNumber(num: string): string {
-  const cardNumber = num ? num.replace(/[^\d]/g, '') : '0000000000000000'
+  const isValidNumber = !!num && num.length === 16
+  const cardNumber = isValidNumber ? num : '0000000000000000'
 
-  return cardNumber.replace(MATCHER, (regex, $1, $2, $3, $4) =>
-    [$1, $2, $3, $4].filter((group) => !!group).join(' - '),
-  )
+  const groups = cardNumber.match(CARD_NUMBER_REGEX)?.slice(1) ?? []
+  return groups.filter(Boolean).join(' - ')
 }
