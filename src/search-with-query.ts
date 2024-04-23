a56fe08f0c3b3d435823a87acd7c2afe16391cd6
Jamal <jamalrahimzadegan@gmail.com>
2024-04-23 11:10:57 +0100

    Update search-with-query

--- src/search-with-query.ts
+++ src/search-with-query.ts
@@ -3,8 +3,7 @@ function searchWithQuery(str: string, cb?: () => void) {
 
     const searchParams = new URLSearchParams(window.location.search)
     searchParams.set('search', str)
-    const newPathQuery =
-      window.location.pathname + '?' + searchParams.toString()
+    const newPathQuery = window.location.pathname + '?' + searchParams.toString()
     history.pushState(null, '', newPathQuery)
 
     cb?.()
