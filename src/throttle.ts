a1fae2fabd4615d98946de903db4e8e45d2620dd
Jamal Rahimzadegan <jamalrahimzadegan@gmail.com>
2026-01-16 15:45:12 +0000

    Update throttle function signature to use void

--- src/throttle.ts
+++ src/throttle.ts
@@ -1,7 +1,7 @@
 // Throttle: Executes immediately, then ignores all calls for X time - > Like an elevator—waits for the last person to get on before moving.
 // Debounce: Ignores all calls until there is a "silence" of at least X time. -> Like a security guard—lets one person in, then closes the door for 300ms. -> Example: "Double-Click" Prevention
 
-export default function throttle(fn: Function, timeout: number = 300) {
+export default function throttle(fn: ()=>void, timeout: number = 300) {
   let timer: ReturnType<typeof setTimeout> | undefined;
 
   return (...args) => {
